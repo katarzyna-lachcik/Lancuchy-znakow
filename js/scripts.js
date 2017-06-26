8a1cca333caa89cfb75dacb3f8324f1fd799d24e
Katarzyna Lachcik <kaisek@poczta.fm>
2017-06-26 14:48:14 +0100

    dinosaurUpperCased

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period';
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 var dinosaur = 'triceratops';
 
 var dinosaurUpperCased = dinosaur.toUpperCase();
 console.log(dinosaurUpperCased);
 
-var nameAfter = text.replace('Velociraptor', 'Triceratops');
+var nameAfter = text.replace('Velociraptor', dinosaurUpperCased);
 console.log(nameAfter);
 
-var halfSentence = nameAfter.substr(1, nameAfter.length / 2); 
+var halfSentence = nameAfter.substr(0, nameAfter.length / 2); 
 console.log(halfSentence);
-
